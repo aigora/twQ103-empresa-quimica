7bf323e0669f820b64d30f372b8c865b9c23fd59
carmenmoujarillo <37289816+carmenmoujarillo@users.noreply.github.com>
2019-05-08 01:13:07 +0200

    funciones nuevas

---
+++ trabajo2.c.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+// introducimos las dos estructuras de vectores
+//estructura de la ficha de los trabajadores
+struct trabajador {
+	char nombre[50];
+	char apellido[50];
+	int phone;
+	int dni;
+};
+//estructura del almacenamiento de los productos
+ struct producto  
+ {
+ 	int codigo;
+ 	int unidades;
+} ;
+//FUNCIONES
+//funcion que inicializa el menu
+int menu();
+
+int main()
+{
+	//iniciamos el vector de estructura llamado plantilla
+	 struct trabajador plantilla[100];
+	int opcion,codigo,unidades, trabajadores=0,i;
+	int decision;
+	char despedido[40];
+	FILE *fichero;
+	struct producto A={1515,100}, B={9191,250}, C={3131,1250};
+		do
+	{		
+		menu();
+		printf("elije una opcion: \n");
+		fflush(stdin);
+		scanf("%d",&opcion);
+		switch (opcion)
+		{
+			case 1:
+				//abrimos y leemos el fichero plantilla y cuenta el numero de trabajadores
+				fichero = fopen("plantilla.txt", "r");
+					while (fscanf(fichero,  "%s %s %d %d", plantilla[trabajadores].nombre,plantilla[trabajadores].apellido, &plantilla[trabajadores].phone, &plantilla[trabajadores].dni) != EOF){
+						trabajadores++;
+						}
+				fclose(fichero);
+				// sentencias para contratar personal
+				// nombre, apellidos, dni, telefono
+				printf("Nombre: \n");
+				scanf("%s", plantilla[trabajadores].nombre);
+				printf("Apellido: \n");
+				scanf("%s", plantilla[trabajadores].apellido);
+				printf("Telefono: \n");
+				scanf("%d", &plantilla[trabajadores].phone);
+				printf("DNI (sin letra): \n");
+				scanf("%d", &plantilla[trabajadores].dni);
+				trabajadores++;
+				
+					//grabar los datos en el fichero
+					fichero = fopen("plantilla.txt", "w");
+						for (i=0; i<trabajadores; i++) 
+						{
+							fprintf(fichero, "%s\t", plantilla[i].nombre);
+							fprintf(fichero, "%s\t",plantilla[i].apellido);		
+							fprintf(fichero, "%d\t",plantilla[i].phone);
+							fprintf(fichero, "%d\n",plantilla[i].dni);		
+						}
+					fclose(fichero);
+					trabajadores=0;
+				
+					break;
+
+			case 2:
+				// sentencias para despedir personal
+					fichero = fopen("plantilla.txt", "r");
+					while (fscanf(fichero,  "%s %s %d %d", plantilla[trabajadores].nombre,plantilla[trabajadores].apellido, &plantilla[trabajadores].phone, &plantilla[trabajadores].dni) != EOF){
+					trabajadores++;
+					}
+					fclose(fichero);
+						if (trabajadores == 0){
+							printf("no tienes trabajadores, contratalos\n");
+						}
+		/*	printf("introduzca el nombre del que deseas despedir: \n");
+			scanf("%s",despedido);
+			for(i=0;i<trabajadores;i++){
+				if(despedido==plantilla[i].nombre){
+					return 0;
+				}
+				else
+				return 1;
+			}*/
+					break;
+			case 3:
+				// sentencias para vender productos
+				printf("introduzca el codigo y cuantas unidades quiere:\n");
+				scanf("%d %d", &codigo, &unidades);
+					if(codigo==A.codigo)
+						{
+							if(unidades<=A.unidades)
+								A.unidades-=unidades;
+							else
+								printf("No hay unidades suficientes\n");
+						}
+
+					else if(codigo==B.codigo)
+						{
+							if(unidades<=B.unidades)
+								B.unidades-=unidades;
+							else 
+								printf("No hay unidades suficientes\n");
+						}
+					else if(codigo==C.codigo)
+					{
+						if(unidades<=C.unidades)
+							C.unidades-=unidades;
+						else 
+							printf("No hay unidades suficientes\n");
+					}
+						else
+							printf("el codigo no es correcto\n");
+			break;
+			case 4:
+				// sentencias para comprar productos
+				printf("introduzca el codigo del producto y cuantas unidades quiere comprar:\n");
+				scanf("%d %d", &codigo, &unidades);
+					if(codigo==A.codigo)
+					{
+						A.unidades+=unidades;
+					}
+					else if(codigo==B.codigo)
+					{
+						B.unidades+=unidades;
+					}
+					else if(codigo==C.codigo)
+					{
+					if(unidades<=C.unidades)
+						C.unidades+=unidades;
+					}
+					else
+						printf("el codigo no es correcto\n");
+			break;
+			case 5:
+				printf("-------------------------------------\n");	
+				printf("%-10s\t %-5s\t %-10s\n","Compuesto", "codigo", "unidades");
+				printf("-------------------------------------\n");
+				printf("acido sulfurico\t %d\t %d \n",A.codigo,A.unidades);
+				printf("acido nitrico\t %d\t %d \n",B.codigo,B.unidades);
+				printf("etanol\t\t %d\t %d \n",C.codigo,C.unidades);
+				printf("-------------------------------------\n");
+			break;
+			case 6:
+				fichero = fopen("plantilla.txt", "r");
+					while (fscanf(fichero,  "%s %s %d %d", plantilla[trabajadores].nombre,plantilla[trabajadores].apellido, &plantilla[trabajadores].phone, &plantilla[trabajadores].dni) != EOF){
+					trabajadores++;
+					}
+			fclose(fichero);
+				printf("-----------------------------------------------------------\n");	
+				printf("%-10s\t %-10s\t %-10s\t %-10s\n","nombre", "apellido", "telefono", "DNI");
+				printf("-----------------------------------------------------------\n");
+				for (i=0; i<trabajadores; i++) {
+				printf("%-10s\t", plantilla[i].nombre);
+				printf("%-10s\t",plantilla[i].apellido);		
+				printf("%d\t",plantilla[i].phone);
+				printf("%d\n",plantilla[i].dni);	
+			}
+				printf("-----------------------------------------------------------\n");
+			trabajadores=0;
+			}
+	}while (opcion != 7);
+
+	return 0;
+
+}
+
+int menu(){
+		printf("...............MENU DE OPCIONES.................\n");
+		printf("\t1-contratar personal\n \t2-despedir personal\n \t3-vender productos\n \t4-comprar reactivos\n \t5-estado almacen\n\t6-Mostrar lista de trabajadores\n\t7-salir del programa\n");
+		
+		return 0;
+}
+
+
