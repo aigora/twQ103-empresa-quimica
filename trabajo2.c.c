4d89458b8e0595c9681c875383ae467d19caecfe
carmenmoujarillo <37289816+carmenmoujarillo@users.noreply.github.com>
2019-05-13 21:44:01 +0200

    he realizado el algoritmo para despedir

--- trabajo2.c.c
+++ trabajo2.c.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 // introducimos las dos estructuras de vectores
 //estructura de la ficha de los trabajadores
 struct trabajador {
@@ -6,6 +7,7 @@ struct trabajador {
 	char apellido[50];
 	int phone;
 	int dni;
+	int fuera; // 0=Despedido, 1=contratado
 };
 //estructura del almacenamiento de los productos
  struct producto  
@@ -23,9 +25,10 @@ int main()
 	 struct trabajador plantilla[100];
 	int codigo,unidades, trabajadores=0,i,aux;
 	int decision;
-	int opcion1;
+	int opcion1,j;
 	char despedido[40];
 	FILE *fichero;
+	FILE *despedidos;
 	struct producto A={1515,100}, B={9191,250}, C={3131,1250};
 		do
 	{	
@@ -37,7 +40,11 @@ int main()
 			case 1:
 				//abrimos y leemos el fichero plantilla y cuenta el numero de trabajadores
 				fichero = fopen("plantilla.txt", "r");
-					while (fscanf(fichero,  "%s %s %d %d", plantilla[trabajadores].nombre,plantilla[trabajadores].apellido, &plantilla[trabajadores].phone, &plantilla[trabajadores].dni) != EOF){
+					if(fichero == NULL){
+						printf("Error al abrir el fichero\n");
+						return -1;
+					}
+					while (fscanf(fichero,  "%s %s %d %d %d", plantilla[trabajadores].nombre,plantilla[trabajadores].apellido, &plantilla[trabajadores].phone, &plantilla[trabajadores].dni, &plantilla[trabajadores].fuera) != EOF){
 						trabajadores++;
 						}
 				fclose(fichero);
@@ -53,12 +60,17 @@ int main()
 				
 					//grabar los datos en el fichero
 					fichero = fopen("plantilla.txt", "w");
+					if(fichero == NULL){
+						printf("Error al abrir el fichero\n");
+						return -1;
+					}
 						for (i=0; i<trabajadores; i++) 
 						{
 							fprintf(fichero, "%s\t", plantilla[i].nombre);
 							fprintf(fichero, "%s\t",plantilla[i].apellido);		
 							fprintf(fichero, "%d\t",plantilla[i].phone);
-							fprintf(fichero, "%d\n",plantilla[i].dni);		
+							fprintf(fichero, "%d\n",plantilla[i].dni);	
+							fprintf(fichero, "%d\n", plantilla[i].fuera=1);	
 						}
 					fclose(fichero);
 					trabajadores=0;
@@ -67,15 +79,47 @@ int main()
 
 			case 2:
 				// sentencias para despedir personal
+		
 					fichero = fopen("plantilla.txt", "r");
-					while (fscanf(fichero,  "%s %s %d %d", plantilla[trabajadores].nombre,plantilla[trabajadores].apellido, &plantilla[trabajadores].phone, &plantilla[trabajadores].dni) != EOF){
+					if(fichero == NULL){
+						printf("Error al abrir el fichero\n");
+						return -1;
+					}
+					while (fscanf(fichero,  "%s %s %d %d %d" , plantilla[trabajadores].nombre,plantilla[trabajadores].apellido, &plantilla[trabajadores].phone, &plantilla[trabajadores].dni ,&plantilla[trabajadores].fuera) != EOF){
 					trabajadores++;
 					}
 					fclose(fichero);
 						if (trabajadores == 0){
 							printf("no tienes trabajadores, contratalos\n");
 						}
-	
+					printf("Introduce el nombre del empleado que desee despedir: \n");
+					scanf("%s", despedido);
+					i=0;
+					for(i=0;i<trabajadores;i++){
+							if(strcmp(despedido, plantilla[i].nombre)==0){
+									printf("Vas a despedir a %s\n", plantilla[i].nombre);
+									despedidos=1;
+									plantilla[i].fuera=0;
+							}
+							if(despedidos!=1)
+								printf("No hay ningun empleado con ese nombre\n");
+						}
+						fichero=fopen("plantilla.txt", "w");
+						if(fichero == NULL){
+						printf("Error al abrir el fichero\n");
+						return -1;
+						i=0;
+						}
+							for (i=0; i<trabajadores; i++) 
+						{
+							fprintf(fichero, "%s\t", plantilla[i].nombre);
+							fprintf(fichero, "%s\t",plantilla[i].apellido);		
+							fprintf(fichero, "%d\t",plantilla[i].phone);
+							fprintf(fichero, "%d\t",plantilla[i].dni);	
+							fprintf(fichero, "%d\n", plantilla[i].fuera);	
+						}
+						trabajadores=0;
+						fclose(fichero);
 					break;
 			case 3:
 				// sentencias para vender productos
@@ -137,36 +181,39 @@ int main()
 			break;
 			case 6:
 				fichero = fopen("plantilla.txt", "r");
-					while (fscanf(fichero,  "%s %s %d %d", plantilla[trabajadores].nombre,plantilla[trabajadores].apellido, &plantilla[trabajadores].phone, &plantilla[trabajadores].dni) != EOF){
+					if(fichero == NULL){
+						printf("Error al abrir el fichero\n");
+						return -1;
+					}
+					while (fscanf(fichero,  "%s %s %d %d %d", plantilla[trabajadores].nombre,plantilla[trabajadores].apellido, &plantilla[trabajadores].phone, &plantilla[trabajadores].dni, &plantilla[trabajadores].fuera) != EOF){
 					trabajadores++;
 					}
 			fclose(fichero);
-				printf("-----------------------------------------------------------\n");	
-				printf("%-10s\t %-10s\t %-10s\t %-10s\n","nombre", "apellido", "telefono", "DNI");
-				printf("-----------------------------------------------------------\n");
+				printf("------------------------------------------------------------------------------------\n");	
+				printf("%-10s\t %-10s\t %-10s\t %-10s\t %-10s\n","nombre", "apellido", "telefono", "DNI","despedido(0=si,1=no)");
+				printf("----------------------------------------------------------------------\n");
 				for (i=0; i<trabajadores; i++) {
 				printf("%-10s\t", plantilla[i].nombre);
 				printf("%-10s\t",plantilla[i].apellido);		
 				printf("%d\t",plantilla[i].phone);
-				printf("%d\n",plantilla[i].dni);	
+				printf("%d\t",plantilla[i].dni);
+				printf("%d\n", plantilla[i].fuera);	
 			}
-				printf("-----------------------------------------------------------\n");
+				printf("-----------------------------------------------------------------------------------\n");
 			trabajadores=0;
-			}
-	}while (opcion1 != 7);
+		
+	}
+	}while(opcion1 != 7);
 
 	return 0;
-
 }
 
 int menu(){
 		int opcion;
 		printf("...............MENU DE OPCIONES.................\n");
 		printf("\t1-contratar personal\n \t2-despedir personal\n \t3-vender productos\n \t4-comprar reactivos\n \t5-estado almacen\n\t6-Mostrar lista de trabajadores\n\t7-salir del programa\n");
-	printf("Introduce una de estas opciones\n");
-
-	scanf("%d", &opcion);
-	
-	return opcion;
+		printf("Introduce una de estas opciones\n");
+		scanf("%d", &opcion);	
+		return opcion;
 }
 
